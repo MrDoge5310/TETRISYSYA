bd1bb9f16957e1f02082d480b3474d7cbe181c47
MrDoge5310 <108025011+MrDoge5310@users.noreply.github.com>
2022-06-25 14:27:20 +0300

    Add files via upload

--- Header.h
+++ Header.h
@@ -2,13 +2,60 @@
 
 #define StartButtonClicked		1
 
+
 HWND hScoreControl;
 HWND hMainWnd;
 HWND hChildWnd;
+HWND hTimer;
 
-LRESULT CALLBACK ChildClassProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+bool GameOver = false;
+bool colided = false;
+int timer = 0;
+
+class SquareClass
+{
+public:
+	int x2;
+	int y2;
+	int x1;
+	int y1;
+
+	SquareClass(int X2, int Y2, int X1, int Y1)
+	{
+		x2 = X2;
+		y2 = Y2;
+		x1 = X1;
+		y1 = Y1;
+	}
+	void Move()
+	{
+		y1 += 20;
+		y2 += 20;
+
+	}
+
+	RECT FillRect(RECT r)
+	{
+		r = { x2, y2, x1, y1 };
+
+		return r;
+	}
+};
+
+RECT square;
+RECT GamingField = { 400, 800, 60, 60 };
+PAINTSTRUCT ps;
+PAINTSTRUCT ps1;
+
+SquareClass s1(250, 60, 200, 110);
+
+HBRUSH Blue = CreateSolidBrush(RGB(0, 0, 150));
+HBRUSH FieldBrush = CreateSolidBrush(RGB(0, 0, 0));
+
 LRESULT CALLBACK MainClassProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH bgcolor, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure);
-WNDCLASS NewChildWindowClass(HBRUSH bgcolor, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure);
+
 
 void MainWndAddWidgets(HWND hWnd);
+void Timer();
+void MainCycle();
--- TetrisCode.cpp
+++ TetrisCode.cpp
@@ -1,24 +1,25 @@
 #include <windows.h>
+#include<string>
+#include <strstream> //for std::strstream
 #include "Header.h"
 #include "resource.h"
+
+
+
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szCmdLine, int nCmdShow)
 {
 	WNDCLASS MainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)),
 		L"MainWndClass", MainClassProcedure);
 
-	WNDCLASS ChildClass = NewChildWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)),
-		L"MainWndClass", ChildClassProcedure);
-
 	if (!RegisterClassW(&MainClass)) { return -1; }
 
 	MSG MainMessage = { 0 };
 
 	hMainWnd = CreateWindow(L"MainWndClass", L"Tetris", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 300, 50, 850, 900, NULL, NULL, NULL, NULL);
-	hChildWnd = CreateWindow(L"MainWndClass", L"Gaming Field", WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CHILD, 10, 10, 200, 200, hMainWnd, NULL , NULL, NULL);
 
-	while (GetMessage(&MainMessage, NULL, NULL, NULL)) 
-	{ 
+	while (GetMessage(&MainMessage, NULL, NULL, NULL))
+	{
 		TranslateMessage(&MainMessage);
 		DispatchMessage(&MainMessage);
 	}
@@ -26,23 +27,9 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szCmdLine, int nC
 	return 0;
 }
 
-WNDCLASS NewChildWindowClass(HBRUSH bgcolor, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure)
-{
-	WNDCLASS NCWC = { 0 };
-
-	NCWC.hCursor = cursor;
-	NCWC.hIcon = icon;
-	NCWC.hInstance = hInst;
-	NCWC.lpszClassName = name;
-	NCWC.hbrBackground = bgcolor;
-	NCWC.lpfnWndProc = procedure;
-
-	return NCWC;
-}
-
 
 WNDCLASS NewWindowClass(HBRUSH bgcolor, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure) {
-	
+
 	WNDCLASS NWC = { 0 };
 
 	NWC.hCursor = cursor;
@@ -55,37 +42,37 @@ WNDCLASS NewWindowClass(HBRUSH bgcolor, HCURSOR cursor, HINSTANCE hInst, HICON i
 	return NWC;
 }
 
-LRESULT CALLBACK ChildClassProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
-	switch (msg) {
-	case WM_CREATE:
-
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-
-	default: return DefWindowProc(hWnd, msg, wp, lp);
-	}
-}
-
 LRESULT CALLBACK MainClassProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 	switch (msg) {
 	case WM_COMMAND:
 		switch (wp) {
 		case StartButtonClicked:
-			SetWindowTextA(hScoreControl, "100");
-			MessageBoxA(hWnd, "Game has started!", "Start", MB_OK);
-			break;
 
-		default: break;
+			while (!GameOver) {
+				Timer();
+				MainCycle();
+			}
 		}
+		break;
+
+	case WM_PAINT:
+
+		BeginPaint(hWnd, &ps);
+		FillRect(ps.hdc, &GamingField, FieldBrush);
+		EndPaint(hWnd, &ps);
+		
+		RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
+
+		square = s1.FillRect(square);
+
+		BeginPaint(hWnd, &ps1);
+		FillRect(ps.hdc, &square, Blue);
+		EndPaint(hWnd, &ps1);
+
 		break;
 
 	case WM_CREATE:
 		MainWndAddWidgets(hWnd);
-
 
 		break;
 
@@ -100,7 +87,30 @@ LRESULT CALLBACK MainClassProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 
 void MainWndAddWidgets(HWND hWnd)
 {
-	 CreateWindowA ("button", "Start", WS_VISIBLE | WS_CHILD | BS_CENTER, 650, 750, 150, 80, hWnd, (HMENU)StartButtonClicked, NULL, NULL);
-	 CreateWindowA("static", "Score:", WS_VISIBLE | WS_CHILD | ES_LEFT, 650, 700, 50, 50, hWnd, NULL, NULL, NULL);
-	 hScoreControl = CreateWindowA("static", "0", WS_VISIBLE | WS_CHILD | ES_LEFT, 700, 700, 50, 50, hWnd, NULL, NULL, NULL);
+	CreateWindowA("button", "Start", WS_VISIBLE | WS_CHILD | BS_CENTER, 550, 720, 150, 80, hWnd, (HMENU)StartButtonClicked, NULL, NULL);
+	CreateWindowA("static", "Score:", WS_VISIBLE | WS_CHILD | ES_LEFT, 550, 670, 50, 20, hWnd, NULL, NULL, NULL);
+	hScoreControl = CreateWindowA("static", "0", WS_VISIBLE | WS_CHILD | ES_LEFT, 600, 670, 100, 20, hWnd, NULL, NULL, NULL);
+	CreateWindowA("static", "Time:", WS_VISIBLE | WS_CHILD | ES_LEFT, 550, 600, 50, 20, hWnd, NULL, NULL, NULL);
+	hTimer = CreateWindowA("static", "0", WS_VISIBLE | WS_CHILD | ES_LEFT, 600, 600, 100, 20, hWnd, NULL, NULL, NULL);
+}
+
+void MainCycle()
+{
+	s1.Move();
+	s1.FillRect(square);
+	RedrawWindow(hMainWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
+}
+
+void Timer()
+{
+		Sleep(1000);
+		timer++;
+
+		std::strstream s;
+		s << timer << "\x00";
+
+		SetWindowTextA(hTimer, s.str());
+
+		if (timer == 10)
+			GameOver = true;
 }
